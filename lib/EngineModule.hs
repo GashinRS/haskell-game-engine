ff8babb35bd8493350741af306e32e8512b18de2
Sheng Tao <shengtao.tian@ugent.be>
2021-11-13 16:35:45 +0100

    engine uitgebreid

--- lib/EngineModule.hs
+++ lib/EngineModule.hs
@@ -5,7 +5,21 @@ module EngineModule
 , X 
 , Y 
 , Coord 
-, Direction)
+, Direction
+, width
+, height
+, north
+, east
+, south
+, west
+, bottom
+, top
+, left
+, right
+, filled
+, emptyBoard
+, adjustSize
+, drawCoord)
  where
 
 import System.Random (StdGen, getStdGen, randomR)
@@ -31,3 +45,62 @@ getRandomNumberInRange stdGen l u = randomR (l, u) stdGen
 screenGreen, screenGray :: Color
 screenGreen = makeColorI 0x6F 0x77 0x5F 0xFF
 screenGray  = makeColorI 0x64 0x6F 0x5D 0XFF
+
+width :: X
+width   = 10 -- de breedte van het bord
+height :: Y
+height  = 20 -- de hoogte van het bord
+dblock :: Int
+dblock  = 12 -- de zijde van 1 blokje (inclusief marge rondom)
+dwidth :: Float
+dwidth  = 10 -- de zijde van 1 blokje (exclusief marge, inclusief randje)
+dinner :: Float
+dinner  = 7 -- de zijde van 1 blokje (enkel het zwarte stuk middenin)
+fscale :: Float
+fscale  = 3 -- algemene schaal van de hele tekening
+north :: Direction
+north = (0, 1)
+east :: Direction
+east = (1, 0)
+south :: Direction
+south = (0, -1)
+west :: Direction
+west = (-1, 0)
+
+bottom, top :: Y
+left, right :: X
+bottom = -(height `div` 2)
+top    = height `div` 2
+left   = -(width `div` 2)
+right  = width `div` 2
+
+filled :: Picture
+filled = Scale fscale fscale $ Pictures[rectangleWire dwidth dwidth,rectangleSolid dinner dinner]
+
+-- Een lege pixel, gecentreerd rond de oorsprong
+empty :: Picture
+empty = Color screenGray filled
+
+-- Maakt een rij van lege pixels
+makeRow :: Y -> Picture
+makeRow y = pictures[drawEmpty (x, y) | x <- [left..right]]
+
+-- Een bord met enkel lege pixels, gecentreerd rond de oorsprong
+emptyBoard :: Picture
+emptyBoard = Pictures[makeRow y | y <- [bottom..top]]
+
+-- Past het coordinaat van een pixel aan om het juist te kunnen translaten
+adjustSize :: Int -> Float
+adjustSize v = fromIntegral (round fscale * dblock * v)
+
+-- Een gevulde/actieve pixel op de locatie aangeduid door de coördinaat.
+drawCoord :: Coord -> Picture
+drawCoord c = drawPicture c filled
+
+-- Een lege pixel op de locatie aangeduid door de coördinaat.
+drawEmpty :: Coord -> Picture
+drawEmpty c = drawPicture c empty
+
+-- Hulpfunctie die gebruikt wordt om zowel tegels als gevulde pixels mee te tekenen
+drawPicture :: Coord -> Picture -> Picture
+drawPicture c = translate (adjustSize $ fst c) (adjustSize $ snd c)
--- src/Main.hs
+++ src/Main.hs
@@ -29,11 +29,6 @@ import Text.ParserCombinators.ReadPrec (prec)
 import System.Random (StdGen, getStdGen, randomR)
 import EngineModule
 
--- type X = Int
--- type Y = Int
--- type Coord = (X, Y)
--- type Direction = Coord
-
 data Game
   = Playing { player :: [Coord],
               direction :: Direction,
@@ -43,65 +38,6 @@ data Game
   | GameOver {
               random :: (Int, StdGen)
             }
-
-width :: X
-width   = 10 -- de breedte van het bord
-height :: Y
-height  = 20 -- de hoogte van het bord
-dblock :: Int
-dblock  = 12 -- de zijde van 1 blokje (inclusief marge rondom)
-dwidth :: Float
-dwidth  = 10 -- de zijde van 1 blokje (exclusief marge, inclusief randje)
-dinner :: Float
-dinner  = 7 -- de zijde van 1 blokje (enkel het zwarte stuk middenin)
-fscale :: Float
-fscale  = 3 -- algemene schaal van de hele tekening
-north :: Direction
-north = (0, 1)
-east :: Direction
-east = (1, 0)
-south :: Direction
-south = (0, -1)
-west :: Direction
-west = (-1, 0)
-
-bottom, top :: Y
-left, right :: X
-bottom = -(height `div` 2)
-top    = height `div` 2
-left   = -(width `div` 2)
-right  = width `div` 2
-
-filled :: Picture
-filled = Scale fscale fscale $ Pictures[rectangleWire dwidth dwidth,rectangleSolid dinner dinner]
-
--- Een lege pixel, gecentreerd rond de oorsprong
-empty :: Picture
-empty = Color screenGray filled
-
--- Maakt een rij van lege pixels
-makeRow :: Y -> Picture
-makeRow y = pictures[drawEmpty (x, y) | x <- [left..right]]
-
--- Een bord met enkel lege pixels, gecentreerd rond de oorsprong
-emptyBoard :: Picture
-emptyBoard = Pictures[makeRow y | y <- [bottom..top]]
-
--- Past het coordinaat van een pixel aan om het juist te kunnen translaten
-adjustSize :: Int -> Float
-adjustSize v = fromIntegral (round fscale * dblock * v)
-
--- Een gevulde/actieve pixel op de locatie aangeduid door de coördinaat.
-drawCoord :: Coord -> Picture
-drawCoord c = drawPicture c filled
-
--- Een lege pixel op de locatie aangeduid door de coördinaat.
-drawEmpty :: Coord -> Picture
-drawEmpty c = drawPicture c empty
-
--- Hulpfunctie die gebruikt wordt om zowel tegels als gevulde pixels mee te tekenen
-drawPicture :: Coord -> Picture -> Picture
-drawPicture c = translate (adjustSize $ fst c) (adjustSize $ snd c)
 
 gamePic :: Game -> Picture
 gamePic (Playing p d a r) = Pictures[emptyBoard, Pictures[drawCoord x | x <- p], drawCoord a]
