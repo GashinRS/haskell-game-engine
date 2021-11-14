103b823e5bbae816d6e5e8fcac830b6bde12b8ff
Sheng Tao <shengtao.tian@ugent.be>
2021-11-14 17:51:45 +0100

    kogels doden tanks

--- lib/Tanks.hs
+++ lib/Tanks.hs
@@ -8,7 +8,7 @@ module Tanks (
 ) where
 
 
-import Data.List (delete, intersect)
+import Data.List (delete, intersect, dropWhile, takeWhile)
 import Graphics.Gloss
     ( Picture(Pictures), play, Display(InWindow) )
 import Graphics.Gloss.Interface.Pure.Game
@@ -56,6 +56,10 @@ test t = fmap (tuplesSum t) allDirections
 getTankCoordinates :: Coord -> Direction -> [Coord]
 getTankCoordinates t d = fmap (tuplesSum t) (removeWindDirections d) ++ [t] ++ [tankHead t d]
 
+-- Dit geeft alle coordinaten terug rond het centrum van een tank
+getTankCollisionZone :: Coord -> [Coord]
+getTankCollisionZone t = fmap (tuplesSum t) allDirections ++ [t]
+
 removeWindDirections :: Direction -> [Direction]
 removeWindDirections d
     | d == north = delete northWest $ delete northEast allDirections
@@ -74,27 +78,43 @@ tankHead = tuplesSum
 shootBullet :: Coord -> Direction -> Coord
 shootBullet t d = tuplesSum (tankHead t d) d
 
--- noTankCollision :: [Coord] -> [Coord] -> Bool
--- noTankCollision t ts = null $ s `intersect` concat [getTankCoordinates t' | t' <- ts]
+noTankCollision :: Coord -> [Coord] -> Bool
+noTankCollision t ts = null $ getTankCollisionZone t `intersect` concat [getTankCollisionZone t' | t' <- ts]
 
-
-moveTank :: Coord -> Direction -> Coord
-moveTank t d
+moveTank :: Coord -> Direction -> [Coord] -> Coord
+moveTank t d ts
     | let n = tuplesSum t d
-          tc = getTankCoordinates n d, all isInBounds tc = n
-    | otherwise                                                      = t
+          tc = getTankCoordinates n d
+          ts' = delete t ts, all isInBounds tc && noTankCollision n ts' = n
+    | otherwise                                                         = t
 
-moveTanks :: [Coord] -> [Direction] -> [Coord]
-moveTanks ts ds = [moveTank (ts !! x) (ds !! x) | x <- [0..length ts - 1]]
+-- Argumenten in volgorde: coordinaat van de speler, coordinaat van de vijanden, de richtingen van de vijanden
+-- Coordinaat van de speler moet meegegeven worden om collision te voorkomen tussen de vijanden en de speler
+moveTanks :: Coord -> [Coord] -> [Direction] -> [Coord]
+moveTanks t ts ds = [moveTank (ts !! x) (ds !! x) (t:ts) | x <- [0..length ts - 1]]
 
-randomDirection :: StdGen -> Direction 
-randomDirection stdGen = 
+changeDirection :: [Direction] -> (Int, StdGen) -> [Direction]
+changeDirection ed r = replaceN ed (fst r) $ randomDirection $ snd r
+
+moveBullets :: [Coord] -> [Direction] -> [Coord]
+moveBullets b bd = [tuplesSum (b !! x) $ bd !! x | x <- [0..length b-1]]
+
+randomDirection :: StdGen -> Direction
+randomDirection stdGen =
     let r = getRandomNumberInRange stdGen 0 $ length tankDirections - 1 in
         tankDirections !! fst r
 
 replaceN :: [a] -> Int -> a -> [a]
 replaceN l 0 a = a:tail l
 replaceN l n a = take(n-1) l ++ [a] ++ drop n l
+
+removeN :: [a] -> Int -> [a]
+removeN l 0 = tail l 
+removeN l n =  take(n-1) l ++ drop n l
+
+-- argumenten in volgorde: de kogels, alle vijanden, de index van de geraakte vijand
+destroyedTankIndex :: [Coord] -> [Coord] -> Int
+destroyedTankIndex b ts = length $ takeWhile (==False) [ null (b `intersect` getTankCollisionZone x) | x <- ts]
 
 startGame :: (Int, StdGen) -> Game
 startGame = Tanks (0, bottom + 1) north [(-3, 6), (3, 6)] [south, south] [] []
@@ -103,19 +123,25 @@ gamePic :: Game -> Picture
 gamePic (Tanks p d e ed b bd r) =
     Pictures[emptyBoard, Pictures[drawCoord x | x <- getTankCoordinates p d], Pictures[drawCoord x | x <- getEnemyTankCoordinates e ed],
     Pictures[drawCoord x | x <- b]]
+gamePic (GameOver r s) = displayMessage $ "Score: " ++ show s
 
 move :: Event -> Game -> Game
-move (EventKey (SpecialKey KeyLeft) Down _ _) (Tanks p d e ed b bd r)   = Tanks (moveTank p west) west e ed b bd r
-move (EventKey (SpecialKey KeyRight) Down _ _) (Tanks p d e ed b bd r)  = Tanks (moveTank p east) east e ed b bd r
-move (EventKey (SpecialKey KeyDown) Down _ _) (Tanks p d e ed b bd r)   = Tanks (moveTank p south) south e ed b bd r
-move (EventKey (SpecialKey KeyUp) Down _ _) (Tanks p d e ed b bd r)     = Tanks (moveTank p north) north e ed b bd r
+move (EventKey (SpecialKey KeyLeft) Down _ _) (Tanks p d e ed b bd r)   = Tanks (moveTank p west e) west e ed b bd r
+move (EventKey (SpecialKey KeyRight) Down _ _) (Tanks p d e ed b bd r)  = Tanks (moveTank p east e) east e ed b bd r
+move (EventKey (SpecialKey KeyDown) Down _ _) (Tanks p d e ed b bd r)   = Tanks (moveTank p south e) south e ed b bd r
+move (EventKey (SpecialKey KeyUp) Down _ _) (Tanks p d e ed b bd r)     = Tanks (moveTank p north e) north e ed b bd r
 move (EventKey (SpecialKey KeySpace ) Down _ _) (Tanks p d e ed b bd r) = Tanks p d e ed (b ++ [shootBullet p d]) (bd ++ [d]) r
 move (EventKey (SpecialKey KeyF5 ) Down _ _) (GameOver r s)             = GameOver r s
 move _ g                                                                = g
 
 next :: Float -> Game -> Game
-next f (Tanks p d e ed b bd r) = let r' = getRandomNumberInRange (snd r) 0 $ length e in
-    Tanks p d (moveTanks e ed) (replaceN ed (fst r') $ randomDirection $ snd r') [tuplesSum (b !! x) $ bd !! x | x <- [0..length b-1]] bd r'
+next f (Tanks p d e ed b bd r)
+    | null e = GameOver r 2
+    | or [x `elem` concat [getTankCollisionZone e' | e' <- e] | x <- b] = Tanks p d (removeN e i) (removeN ed i) b bd r
+    | or [x `elem` getTankCoordinates p d | x <- b] = GameOver r $ 2 - length e
+    | otherwise = Tanks p d (moveTanks p e ed) (changeDirection ed r') (moveBullets b bd) bd r'
+                    where r' = getRandomNumberInRange (snd r) 0 $ length e
+                          i = destroyedTankIndex b e
 next f g = g
 
 tanksMain :: IO ()
